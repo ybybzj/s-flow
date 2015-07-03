7210422604c77e887a8e36398c34cc26fb6f00a0
ybybzj <ybybzj@gmail.com>
2015-07-03 14:58:33 +0800

    update dependencies

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var check = require('@zj/check/type');
+var check = require('check/type');
 var t = require('typology');
-var slice = require('@zj/fnkit/slice');
-var pipe = require('@zj/fnkit/pipe');
+var slice = require('fnkit/slice');
+var pipe = require('fnkit/pipe');
 var Signal = require('./lib/signal');
 var createState = require('./lib/state');
-var Promise = require('@zj/promise');
+var Promise = require('promize');
 var protos = {
   init: function init() {
     this.state = this._createState.apply(this, slice(arguments));
@@ -51,16 +51,18 @@ function SF(opts) {
   var signalCreator = opts.state.options && opts.state.options.signalCreator, sf;
   if (signalCreator) delete opts.state.options.signalCreator;
   sf = Object.create(protos);
-  // sf.state = createState(state.data, state.facets, state.refs, opts);
   sf.signal = new Signal(sf, signalCreator);
   //register signals
   opts.signal(sf.signal);
+  sf.signal.resolve();
   //watch signals
   opts.watch(_watch.bind(sf));
   sf._stateOptions = opts.state;
   return sf;
 }
-
+SF.isCtorOf = function(sf){
+  return t.get(sf) === 'object' && sf.$type === '__$sf$__';
+}
 module.exports = SF;
 
 function _watch(sigName) {
@@ -89,86 +91,3 @@ function _merge(t, o){
   }
   return t;
 }
-//test
-// if (require.main === module) {
-//   var list = [{
-//     id: '0',
-//     msg: [1]
-//   }, {
-//     id: 1,
-//     msg: [0, 2]
-//   }, {
-//     id: '2',
-//     msg: [3]
-//   }];
-//   var messages = [{
-//     id: 0,
-//     content: "message0"
-//   }, {
-//     id: 1,
-//     content: "message1"
-//   }, {
-//     id: 2,
-//     content: "message2"
-//   }, {
-//     id: 3,
-//     content: "message3"
-//   }];
-//   var curItemFacet = {
-//     cursors: {
-//       list: 'list',
-//       messages: 'messages',
-//       curId: 'curItemId'
-//     },
-//     get: function(data, refs) {
-//       // console.log(data);
-//       var item = data.list[refs.list.get(data.curId)];
-//       if (item) {
-//         item = Object.create(item);
-//         item.messages = item.msg.map(function(msgId) {
-//           return data.messages[refs.message.get(msgId)].content;
-//         }).join('\n');
-//       }
-//       return item;
-//     }
-//   };
-//   var refs = {
-//     list: 'list',
-//     message: 'messages'
-//   };
-//   var app = SF({
-//     data: {
-//       list: {},
-//       messages: {},
-//       curItemId: 1
-//     },
-//     facets: {
-//       curItem: curItemFacet
-//     },
-//     refs: refs
-//   }, {
-//     syncwrite: true
-//   });
-//   app.setup(function(app) {
-//     app.signal.add('load');
-//     app.signal.resolve();
-//   }).watch(function(watch) {
-//     watch('load', function(state) {
-//       setTimeout(function() {
-//         // console.log(state);
-//         list.forEach(function(item) {
-//           var ref = state.refs.list.create(item.id);
-//           state.store.set(['list', ref], item);
-//         });
-//         messages.forEach(function(msg) {
-//           var ref = state.refs.message.create(msg.id);
-//           state.store.set(['messages', ref], msg);
-//         });
-//       }, 1000);
-//     });
-//   });
-//   app.state.store.on('update', function() {
-//     console.log(app.state.store.facets.curItem.get());
-//   })
-//   app.signal.get('load')('start');
-// }
